957b1d964af2d0647cda70e3c3892d6a434e7a93
Rich Hillary <rich.hillary@csiro.au>
2023-03-02 09:28:31 +1100

    changes to package version

--- abctuna/inst/include/abctuna.h
+++ abctuna/inst/include/abctuna.h
@@ -1,63 +1,8 @@
-/**
- * @author      : Iago MOSQUEIRA (WMR) (iago.mosqueira@wur.nl)
- * @file        : abctuna
- * @created     : Tuesday Feb 28, 2023 07:30:35 CET
- */
-
-#ifndef ABCTUNA_H
-
-#define ABCTUNA_H
-
-// header for multidimensional arrays
-
-// function decs
-
-double **arr2(int,int);
-double ***arr3(int,int,int);
-double ****arr4(int,int,int,int);
-
-// function defs
-
-double **arr2(int d1,int d2) 
-{
- 
-  double **t = new double*[d1];
-  for(int ii=0;ii<d1;ii++) t[ii] = new double[d2];
-
-  return t;
-
-}
-
-double ***arr3(int d1,int d2,int d3)
-{
-  double ***t = new double**[d1];
-  for(int ii=0;ii<d1;ii++) {
-
-    t[ii] = new double*[d2];
-    for(int jj=0;jj<d2;jj++) t[ii][jj] = new double[d3];
-
-  }
-
-  return t;
-}
-
-double ****arr4(int d1,int d2,int d3,int d4)
-{
-
-  double ****t = new double***[d1];
-  for(int ii=0;ii<d1;ii++) {
-
-    t[ii] = new double**[d2];
-    for(int jj=0;jj<d2;jj++) {
-
-      t[ii][jj] = new double*[d3];
-      for(int kk=0;kk<d3;kk++) t[ii][jj][kk] = new double[d4];
-
-    }
-  }
-
-  return t;
-}
+//////////////////////////////////////////////////////
+// header classes for multidimensional arrays ////////
+//////////////////////////////////////////////////////
+// R. Hillary CSIRO 2023 /////////////////////////////
+//////////////////////////////////////////////////////
 
 class arr2d
 {
@@ -147,7 +92,3 @@ double& arr4d::operator()(int i1,int i2,int i3,int i4)
 {
   return(t[i1][i2][i3][i4]);
 }
-
-
-#endif /* end of include guard ABCTUNA_H */
-
