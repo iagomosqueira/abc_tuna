c91b2f7b1201d9f4ba7f8057763f6543f6e487f8
Rich Hillary <rich.hillary@csiro.au>
2023-03-16 16:29:48 +1100

    vanilla R ABC MCMC

---
+++ v2/abc2.R
@@ -0,0 +1,203 @@
+# abc.R - DESC
+# /home/mosquia/Active/Doing/ABC_tuna+iotc/abc_tuna/v2/abc.R
+
+# Copyright (c) WUR, 2023.
+# Author: Iago MOSQUEIRA (WMR) <iago.mosqueira@wur.nl>
+#
+# Distributed under the terms of the EUPL-1.2
+
+
+# data
+
+#' @param laa Length at ageby sex
+#' @param wal Weight at length by sex
+#' @param mal Maturity at length for females
+#' @param cpues CPUE indices by fishery, area [fishery, year]
+#' @param lenfreq Length-frequency in catch by fishery [len, year, fishery]
+#' @param iALK, VB
+
+# arguments
+
+#' @param priors List for h, K
+#' @param pvars Proposal variances
+#' @param Fstatus F/FMSY prior
+
+library(Rcpp)
+library(FLCore)
+library(ggplotFL)
+library(parallel)
+source("utilities.R")
+
+#sourceCpp("init_pdyn.cpp")
+#sourceCpp("msy_pdyn.cpp")
+#sourceCpp("pdyn.cpp")
+#sourceCpp("pdyn_lfcpue.cpp")
+
+# NC by fleet [y, s, f]
+# 
+
+#load('../data/data.RData')
+load("alb_abcdata.rda")
+
+# fnscale 
+fnscale <- 1
+
+# years and values for stock status priors
+
+#yfmsy <- c(1,2,ny-1,ny) # first two, last two 
+#mufmsy <- c(0.6,0.55,0.57,0.6)
+#sdfmsy <- c(0.125,0.125,0.175,0.18)
+ybmsy <- c(ny-1,ny)
+mubmsy <- c(2.25,2)
+sdbmsy <- c(0.35,0.35)
+ydep <- 1
+mudep <- 0.5
+sddep <- 0.1
+
+# recale weight to tonnes
+
+wta <- wta[]*1e-3
+ 
+pobs <- apply(LFfits,2,function(x){x <- x/sum(x)}) # what we will fit to
+
+# --- simulator
+
+# - arguments
+#   - parameters: R0, dep, h, epsr, selpars
+#   - biology
+#   - fishery
+
+R0 <- 17e6
+dep <- 0.5
+h <- 0.8
+
+# number of fisheries
+
+nf <- dim(C)[3]
+
+# number of distinct selectivity groups
+
+nselg <- 5
+
+# selectivity for each fishery
+
+selidx <- c(1,2,3,4,5,5)
+
+# fisheries with LF data
+
+flf <- c(1,2,3,4,6)
+nflf <- length(flf)
+pobs <- apply(LFfits,2,function(x){x <- x/sum(x)}) # what we will fit to
+pobs <- pobs[,flf]
+
+# catch distro targets
+
+pctarg <- C[1,,] / sum(C[1,,])
+
+# set up selectivity parameters (all double normal)
+
+smax <- c(120,125,85,85,115)
+sL <- c(20,20,7,7,10)
+sR <- c(35,30,25,15,30)
+selpars <- cbind(smax,sL,sR)
+
+# recruitment variations (ny-1)
+
+epsr <- rep(0,ny-1)
+
+# recruitment season
+
+srec <- 4
+
+# sex ratio at birth (fiddy:fiddy)
+
+psi <- 0.5
+
+# dimensions
+
+dms <- c(na,ns,nf,nselg)
+
+## MCMC algorithm 
+
+# Gibbs sampling parameter groupings
+# 1. B0 and dep
+# 2. recruitment deviates
+# 3. selectivity 
+
+npar <- 2+ny-1+3*nselg
+ngibbs <- 3
+paridx <- list()
+paridx[[1]] <- 1:2
+paridx[[2]] <- 3:(ny+1)
+paridx[[3]] <- (ny+2):npar
+lidx <- unlist(lapply(paridx,length))
+
+# SD in CPUE index
+
+fcpue <- 1
+scpue <- 1:4
+sd.cpue <- rep(NA,length(scpue))
+par(mfrow=c(2,2))
+for(s in scpue) {
+
+  idf <- data.frame(t=yrs,y=log(I[,s,fcpue]))
+  ires <- loess(y~t,idf)
+  plot(yrs,ires$fitted,type='l')
+  points(yrs,log(I[,s,fcpue]))
+  sd.cpue[s] <- sd(residuals(ires))
+
+}
+
+sdcpue <- mean(sd.cpue)
+
+# sigmaR
+
+sigmar <- 0.3 # from assessment
+
+# KLmax
+
+KLmax <- 0.8 # consistent with minimum Neff = 20 multinomial
+
+# seasonal q for CPUE (T or F)
+
+seasonq <- FALSE
+
+# burn-in and thinning factor
+ 
+burn <- 50
+thin <- 10
+
+###################
+# run the sampler #
+###################
+
+# set up initial guess parameter vector
+
+parvecold <- c(log(R0),logit(dep),epsr,log(as.vector(selpars)))
+
+# RW variance by Gibbs grouping
+
+rwsd <- rep(0,npar)
+rwsd[paridx[[1]]] <- c(0.15,0) 
+rwsd[paridx[[2]]] <- 0.15
+rwsd[paridx[[3]]] <- 0.035
+
+nits <- 10 # total number of retained samples
+system.time(zzz <- rmcmc.abc(nits))
+zzz$acp/nits
+boxplot(zzz$pars,outline=F,col='magenta')
+
+# parallelised efficient version
+
+parvecold <- zzz$pars[nits,]
+nits <- 100
+ncore <- 10
+mcnits <- floor(nits/ncore)
+system.time(mczzz <- mclapply(rep(mcnits,ncore),rmcmc.abc,mc.cores=ncore))
+
+mcacp <- apply(matrix(unlist(lapply(mczzz,function(x){x <- x$acp})),ncol=ngibbs,byrow=T),2,sum)/nits
+mcacp
+mcpars <- mczzz[[1]]$pars
+for(i in 2:ncore) mcpars <- rbind(mcpars,mczzz[[i]]$pars)
+boxplot(mcpars,outline=F,col='magenta') 
+
--- v2/utilities.R
+++ v2/utilities.R
@@ -80,6 +80,14 @@ msyfn <- function(H,ph,sela) {
   return(sum(resx$C))
 }
 
+rmsyfn <- function(H,ph,sela) {
+
+  hx <- H * ph
+  resx <- rmsypdyn(c(ns,na,nf),srec,R0,h,psi,M,mata,wta,sela,hx)
+    
+  return(sum(resx$C))
+}
+
 # vanilla R initial popn dynamics
 
 # rinitpdyn {{{
@@ -101,17 +109,19 @@ rinitpdyn <- function(dms,srec,psi,M,mata,wta,sela,hstart) {
 
   for(s in 1:ns) {
 
-    if(s < srec) neqm[0,s,] <- 0
-    if(s == srec) neqm[0,s,] <- c(psi,1-psi)
-    if(s > srec) neqm[0,s,] <- n[0,s-1,]*exp(-M)
+    if(s < srec) neqm[1,s,] <- 0
+    if(s == srec) neqm[1,s,] <- c(psi,1-psi)
+    if(s > srec) neqm[1,s,] <- neqm[1,s-1,]*exp(-M)
     
   }
 
   for(a in 2:na) {
-  
-    if(s == 1) neqm[a,1,] <- neqm[a-1,ns,]*exp(-M)
-    if(s > 1) neqm[a,s,] <- neqm[a,s-1,]*exp(-M)
+    for(s in 1:ns) {
 
+      if(s == 1) neqm[a,1,] <- neqm[a-1,ns,]*exp(-M)
+      if(s > 1) neqm[a,s,] <- neqm[a,s-1,]*exp(-M)
+
+    }
   }
 
   spr0 <- sum(neqm[,spwn,1]*mata[,spwn,1]*wta[,spwn,1])
@@ -132,11 +142,11 @@ rinitpdyn <- function(dms,srec,psi,M,mata,wta,sela,hstart) {
 
   for(s in 1:ns) {
 
-    if(s < srec) neqm[0,s,] <- 0
-    if(s == srec) neqm[0,s,] <- c(psi,1-psi)
+    if(s < srec) neqm[1,s,] <- 0
+    if(s == srec) neqm[1,s,] <- c(psi,1-psi)
     if(s > srec) {
 
-      for(g in 1:2) neqm[0,s,g] <- n[0,s-1,g]*exp(-M)*(1-hsum[0,s-1,g])
+      for(g in 1:2) neqm[1,s,g] <- neqm[1,s-1,g]*exp(-M)*(1-hsum[1,s-1,g])
 
     }
   }
@@ -169,10 +179,406 @@ rinitpdyn <- function(dms,srec,psi,M,mata,wta,sela,hstart) {
   # SPR ratio
 
   rho <- sprf/spr0
-  return(list(rho=rho,C=ceqm))
+  return(list(rho=rho,C=ceqm,N=neqm))
 }
 # }}}
 
+# {{{ rmsypdyn
+rmsypdyn <- function(dms,srec,R0,h,psi,M,mata,wta,sela,hx) {
+
+  spwn <- ifelse(srec == 1,ns,srec-1)
+
+  ns <- dms[1]
+  na <- dms[2]
+  nf <- dms[3]
+  H <- array(hx,dim=c(ns,nf))
+
+  # unfished level
+
+  neqm <- array(dim=c(na,ns,2))
+  ceqm <- array(dim=c(ns,nf))
+  heqm <- array(dim=c(na,ns,2,nf))
+
+  for(s in 1:ns) {
+
+    if(s < srec) neqm[1,s,] <- 0
+    if(s == srec) neqm[1,s,] <- c(psi,1-psi)
+    if(s > srec) neqm[1,s,] <- neqm[1,s-1,]*exp(-M)
+    
+  }
+
+  for(a in 2:na) {
+    for(s in 1:ns) {
+
+      if(s == 1) neqm[a,1,] <- neqm[a-1,ns,]*exp(-M)
+      if(s > 1) neqm[a,s,] <- neqm[a,s-1,]*exp(-M)
+
+    }
+  }
+
+  spr0 <- sum(neqm[,spwn,1]*mata[,spwn,1]*wta[,spwn,1])
+
+  # exploited eqm
+
+  for(s in 1:ns) {
+    for(g in 1:2) {
+      for(f in 1:nf) {
+        
+        heqm[,s,g,f] <- H[s,f]*sela[,s,g,f]
+
+      }
+    }
+  }
+
+  hsum <- apply(heqm,1:3,sum)
+
+  for(s in 1:ns) {
+
+    if(s < srec) neqm[1,s,] <- 0
+    if(s == srec) neqm[1,s,] <- c(psi,1-psi)
+    if(s > srec) {
+
+      for(g in 1:2) neqm[1,s,g] <- neqm[1,s-1,g]*exp(-M)*(1-hsum[1,s-1,g])
+
+    }
+  }
+
+  for(a in 2:na) {
+    for(s in 1:ns) {
+      for(g in 1:2) {
+      
+        htmp <- min(0.9,hsum[a,s,g])
+        if(s == 1) neqm[a,1,g] <- neqm[a-1,ns,g]*exp(-M)*(1-htmp)
+        if(s > 1) neqm[a,s,g] <- neqm[a,s-1,g]*exp(-M)*(1-htmp)
+
+      }
+    }
+  }
+ 
+  sprf <- sum(neqm[,spwn,1]*mata[,spwn,1]*wta[,spwn,1]) 
+
+  # S-R bit
+
+  rho <- sprf/spr0
+  B0 <- R0*spr0
+  alp <- 4*h/(spr0*(1-h))
+  bet <- (5*h-1)/(B0*(1-h))
+  Bbar <- ifelse((alp*sprf-1.)/bet > 0,(alp*sprf-1)/bet,0)
+  Rbar <- Bbar/sprf
+
+  # scale population by Rbar
+
+  neqm[] <- neqm[]*Rbar
+
+  # catch biomass by fleet
+
+  for(s in 1:ns) {
+    for(f in 1:nf) {
+
+      ceqm[s,f] <- 0
+      for(g in 1:2) ceqm[s,f] <- ceqm[s,f]+sum(neqm[,s,g]*wta[,s,g]*sela[,s,g,f]*H[s,f])
+
+    }
+  } 
+ 
+  return(list(rho=rho,C=ceqm,N=neqm,spr0=spr0,Bmsy=Bbar,Rmsy=Rbar))
+
+}
+# }}}
+
+# rpdynlfcpue {{{
+rpdynlfcpue <- function(dms,srec,R0,h,psi,epsr,spr0,M,mata,wta,sela,Ninit,C,pla,fcpue) {
+
+  ny <- dms[1]
+  ns <- dms[2]
+  na <- dms[3]
+  nl <- dms[4]
+  nf <- dms[5]
+
+  spwn <- ifelse(srec == 1,ns,srec-1)
+
+  B0 <- R0*spr0
+  alp <- 4*h/(spr0*(1-h))
+  bet <- (5*h-1)/(B0*(1-h)) 
+
+  N <- array(dim=c(ny,na,ns,2))
+  H <- array(dim=c(ny,ns,nf))
+  S <- I <- array(dim=c(ny,ns))
+  LF <- array(dim=c(ny,nl,ns,nf))
+
+  # initial population
+
+  N[1,,,] <- Ninit
+  for(s in 1:ns) S[1,s] <- sum(N[1,,s,1]*mata[,s,1]*wta[,s,1])
+  for(f in 1:nf) {
+    for(s in 1:ns) {
+
+      xsum <- 0
+      for(g in 1:2) xsum <- xsum+sum(N[1,,s,g]*sela[,s,g,f]*wta[,s,g])
+      H[1,s,f] <- min(C[1,s,f]/xsum,0.9)
+      if(f == fcpue) I[1,s] <- xsum
+
+      # LF data
+ 
+      for(l in 1:nl) {
+
+        LF[1,l,s,f] <- 0 
+        for(g in 1:2) LF[1,l,s,f] <- LF[1,l,s,f]+sum(N[1,,s,g]*sela[,s,g,f]*pla[l,,s,g])
+      }
+    }
+  }
+
+  # annual loop
+
+  for(y in 2:ny) {
+
+    ysp <- ifelse(srec == 1,y-1,y)
+
+    for(s in 1:ns) {
+
+      if(s < srec) N[y,1,s,] <- 0
+      if(s == srec) {
+
+        Rtot = (alp*S[ysp,spwn]/(1+bet*S[ysp,spwn]))*exp(epsr[y-1])
+        N[y,1,s,] <- Rtot*c(psi,1-psi)
+
+      }
+
+      if(s > srec) {
+
+        for(g in 1:2) {
+
+          hsum <- min(sum(H[y,s-1,]*sela[1,s-1,g,]),0.9)
+          N[y,1,s,g] <- N[y ,1,s-1,g]*exp(-M)*(1-hsum)
+
+        }
+      }
+
+      # loop through ages
+
+      for(a in 2:na) {
+        for(g in 1:2) {
+
+          if(s == 1) {
+
+            hsum <- min(sum(H[y-1,s-1,]*sela[a-1,ns,g,]),0.9)
+            N[y,a,s,g] <- N[y-1,a-1,ns,g]*exp(-M)*(1-hsum) 
+ 
+          } else {
+  
+            hsum <- min(sum(H[y,s-1 ,]*sela[a,s-1,g,]),0.9) 
+            N[y,a,s,g] <- N[y,a,s-1,g]*exp(-M)*(1-hsum) 
+
+          }
+        }
+      }
+
+      # female SSB
+  
+      S[y,s] <- sum(N[y,,s,1]*mata[,s,1]*wta[,s,1]) 
+
+      # harvest rates, CPUE and LF 
+  
+      for(f in 1:nf) {
+
+        xsum <- 0
+        for(g in 1:2) xsum <- xsum+sum(N[y,,s,g]*sela[,s,g,f]*wta[,s,g])
+        H[y,s,f] <- min(C[y,s,f]/xsum,0.9)
+        if(f == fcpue) I[y,s] <- xsum
+
+        # LF data
+ 
+        for(l in 1:nl) {
+
+          LF[y,l,s,f] <- 0 
+          for(g in 1:2) LF[y,l,s,f] <- LF[y,l,s,f]+sum(N[y,,s,g]*sela[,s,g,f]*pla[l,,s,g])
+        } 
+      }
+    }
+  }
+
+  return(list(S=S,N=N,H=H,I=I,LF=LF))
+}
+# }}}
+
+# rmcmc.abc {{{
+rmcmc.abc <- function(nits) {
+
+  theta.mcmc <- matrix(nrow=nits,ncol=npar)
+  acp <- rep(0,ngibbs)
+
+  # get initial guess discrepancy
+
+  xx <- rsim(R0,dep,h,M,selpars,epsr,dms,pctarg,selidx) 
+
+  # LF discrepancy
+
+  phat <- xx$LF
+  kllf <- pobs*log(pobs/phat)
+  dlf <- sum(apply(kllf,2,function(x){sum(x[!is.nan(x)])}))
+
+  # CPUE discrepancy
+
+  if(seasonq) {
+
+    resq <- log(I[,,fcpue]/xx$I)
+    lnq <- apply(resq,2,mean)
+    resq <- t(apply(resq,1,function(x,lnq){x <- x-lnq},lnq))
+
+  } else {
+
+    resq <- log(I[,,fcpue]/xx$I)
+    lnq <- mean(resq)
+    resq <- resq-lnq
+
+  }
+
+  dcpue <- sum(dnorm(resq,0,sdcpue,TRUE))
+
+  ## priors (parameters + stock status)
+
+  # status priors
+
+  Bmsyrat <- xx$S[,3]/xx$Bmsy
+  Bmsyrat <- Bmsyrat[ybmsy]
+  dSSB <- xx$S[,srec-1]/xx$B0
+  dSSB <- dSSB[ydep]
+
+  if(length(ybmsy) == 1) {
+
+    sprior <- dnorm(Bmsyrat,mubmsy,sdbmsy,TRUE)
+
+  } else {
+
+    sprior <- sum(dnorm(Bmsyrat,mubmsy,sdbmsy,TRUE))
+
+  }
+
+  if(length(ydep) == 1) {
+
+    sprior <- sprior+dnorm(dSSB,mudep,sddep,TRUE)
+
+  } else {
+
+    sprior <- sprior+sum(dnorm(dSSB,mudep,sddep,TRUE))
+  }  
+
+  # parameter priors
+
+  pprior <- sum(dnorm(epsr,0,sigmar,TRUE))
+
+  # starting discrepancy
+
+  dtotold <- dcpue+sprior+pprior-dlf
+
+  for(n in 1:(burn+thin*nits)) {
+
+    for(gg in 1:ngibbs) {
+
+      epsrw <- rnorm(lidx[gg],0,rwsd[paridx[[gg]]])
+      parvecnew <- parvecold
+      parvecnew[paridx[[gg]]] <- parvecnew[paridx[[gg]]]+epsrw
+      R0x <- exp(parvecnew[1])
+      depx <- ilogit(parvecnew[2])
+      epsrx <- parvecnew[3:(ny+1)]
+      selvx <- exp(parvecnew[(ny+2):npar])
+      selparsx <- cbind(selvx[1:nselg],selvx[(nselg+1):(2*nselg)],selvx[(2*nselg+1):(3*nselg)])
+      xx <- rsim(R0x,depx,h,M,selparsx,epsrx,dms,pctarg,selidx)
+
+      # LF discrepancy
+
+      phat <- xx$LF
+      kllf <- pobs*log(pobs/phat)
+      dlf <- sum(apply(kllf,2,function(x){sum(x[!is.nan(x)])}))
+
+      # CPUE discrepancy
+
+      if(seasonq) {
+
+        resq <- log(I[,,fcpue]/xx$I)
+        lnq <- apply(resq,2,mean)
+        resq <- resq-lnq
+
+      } else {
+
+        resq <- log(I[,,fcpue]/xx$I)
+        lnq <- mean(resq)
+        resq <- resq-lnq
+
+      }
+
+      dcpue <- sum(dnorm(resq,0,sdcpue,TRUE))
+
+      ## priors (parameters + stock status)
+
+      # status priors
+
+      Bmsyrat <- xx$S[,3]/xx$Bmsy
+      Bmsyrat <- Bmsyrat[ybmsy]
+      dSSB <- xx$S[,srec-1]/xx$B0
+      dSSB <- dSSB[ydep]
+
+      if(length(ybmsy) == 1) {
+
+        sprior <- dnorm(Bmsyrat,mubmsy,sdbmsy,TRUE)
+
+      } else {
+
+        sprior <- sum(dnorm(Bmsyrat,mubmsy,sdbmsy,TRUE))
+
+      }
+
+      if(length(ydep) == 1) {
+
+        sprior <- sprior+dnorm(dSSB,mudep,sddep,TRUE)
+
+      } else {
+
+        sprior <- sprior+sum(dnorm(dSSB,mudep,sddep,TRUE))
+
+      } 
+
+      # parameter priors
+
+      pprior <- sum(dnorm(epsr,0,sigmar,TRUE))
+
+      ## ABC accept/reject:
+      # 1. KL(LF data) < KL_max or reject immediately
+      # 2. If 1 is true accept/reject given remaining discrepancy
+
+      if(dlf < KLmax) {
+
+        dtotnew <- dcpue+sprior+pprior-dlf
+        pirat <- min(dtotnew-dtotold,0)
+        uvar <- log(runif(1,0,1))
+        accpt <- ifelse(pirat>uvar,TRUE,FALSE)
+
+      } else {
+
+        accpt <- FALSE
+
+      }
+
+      if(accpt) {
+
+        parvecold <- parvecnew
+        dtotold <- dtotnew
+        if(n > burn) acp[gg] <- acp[gg]+1
+
+      }
+    }
+
+    # outputs
+  
+    if(n > burn & (n-burn) %% thin == 0) theta.mcmc[(n-burn)/thin,] <- parvecold
+
+    if(n %% 100 == 0) cat("Iteration",n,"of",burn+nits*thin,"\n")
+ 
+  }
+
+  return(list(pars=theta.mcmc,acp=acp))
+} 
+# }}}
 
 # mcmc.abc {{{
 mcmc.abc <- function(nits) {
@@ -440,6 +846,96 @@ sim <- function(R0=1e6, dep=0.5, h=0.75, M=0.075, selpars, epsr, dms, pctarg,sel
   H <- array(resp2$H,dim=c(ny,ns,nf))
   LFhat <- array(resp2$LF,dim=c(ny,nbins,ns,nf))
   Ihat <- array(resp2$I,dim=c(ny,ns))
+
+  # predicted LF distro for relevant fisheries
+
+  LFhat <- apply(LFhat,c(2,4),sum)
+  phat <- LFhat[,flf]
+  phat <- apply(phat,2,function(x){x <- x/sum(x)})
+
+  return(list(N=N,S=S,H=H,LF=phat,I=Ihat,Bmsy=Bmsy,Cmsy=Cmsy,Hmsy=hmsyv,B0=B0))
+
+}
+# }}}
+
+# rsim {{{
+rsim <- function(R0=1e6, dep=0.5, h=0.75, M=0.075, selpars, epsr, dms, pctarg,selidx) {
+
+  na <- dms[1]
+  ns <- dms[2]
+  nf <- dms[3]
+  nselg <- dms[4]
+
+  # SPR ratio at exploited eqm given steepness and depletion
+  # (based on derivation: dep = (4*h*rho+h-1)/(5*h-1))
+  rhotarg <- (dep*(5*h-1)+1-h)/(4*h)
+
+  # create selectivity-at-age
+
+  sela <- get.sel.age(nf,nselg,selidx,selpars) 
+    
+  # target vector (rho+pc)
+  targv <- logit(c(rhotarg,as.vector(pctarg)))
+
+  # wrapper objective function to solve (minimise)
+
+  # Estimate initial Fs
+  
+  hinit <- 0.15*pctarg[]
+
+  # scalar to give objective function some bite
+
+  theta <- logit(as.vector(hinit))
+
+   zz <- optim(theta,robjfn.init,targv=targv,sela=sela,method=("L-BFGS-B"),control=list(trace=0))
+
+  hinit <- array(ilogit(zz$par), dim=c(ns, nf))
+  resinit <- rinitpdyn(c(ns,na,nf),srec,psi,M,mata,wta,sela,hinit) 
+
+  # relative H-split for MSY calcs
+  ph <- as.vector(hinit[] / sum(hinit))
+
+  msy <- optimise(rmsyfn,interval=c(0,0.7),ph=ph,sela=sela,maximum=TRUE)
+  Hmsy <- msy$maximum
+  Cmsy <- msy$objective
+  resmsy <- rmsypdyn(c(ns,na,nf),srec,R0,h,psi,M,mata,wta,sela,Hmsy*ph)
+  
+  Bmsy <- resmsy$Bmsy
+  spr0 <- resmsy$spr0
+  B0 <- R0*spr0
+  alp <- 4*h/(spr0*(1-h))
+  bet <- (5*h-1)/(B0*(1-h))
+  Bratio <- Bmsy/B0
+  Rratio <- resmsy$Rmsy/R0
+  hmsyv <- apply(array(Hmsy*ph,dim=c(ns,nf)),1,sum)
+
+  #if(!all.equal(Rratio, (4*h*Bratio)/(h*(5*Bratio-1)+1-Bratio)))
+    #warning("B-H invariant check - should be same as Rratio")
+
+  # set up initial numbers-at-age for input to population dynamics
+
+  Rinit <- R0*(4*h*dep)/(h*(5*dep-1)+1-dep)
+  Ninit <- array(resinit$N,dim=c(na,ns,2))
+  Ninit[] <- Ninit[]*Rinit
+
+  # expected catch @ hinit
+  zinit <- rmsypdyn(c(ns,na,nf),srec,R0,h,psi,M,mata,wta,sela,hinit)
+  Cinit <- array(zinit$C,dim=c(ns,nf))
+
+  # main population stuff
+
+  ## generating predicted LF and CPUE 
+    
+  # fishery for CPUE generation
+
+  fcpue <- 1
+  resp2 <- rpdynlfcpue(c(ny,ns,na,nbins,nf),srec,R0,h,psi,epsr,spr0,M,mata,wta,sela,Ninit,C,pla,fcpue)
+
+  N <- resp2$N
+  S <- resp2$S
+  H <- resp2$H
+  LFhat <- resp2$LF
+  Ihat <- resp2$I
 
   # predicted LF distro for relevant fisheries
 
